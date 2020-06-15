11ac43af32694d2cef8ff1c8165b4a405d7bb37f
Marcia Martins <marciapmartins@gmail.com>
2020-06-15 12:46:08 +0100

    Add project files.

---
+++ ByteBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func01 = new Funcionario();
+            func01.Nome = "01 Funcionario";
+            func01.CPF = "000.000.000-01";
+            func01.Salario = 1000.00;
+
+            Console.WriteLine($"Bonificaçao: {func01.GetBonificacao()}");
+            Console.ReadLine();
+        }
+    }
+}
